ecd1568d925bd6046db1d4294a3d44d004149e31
Luca Canali <info@canaliluca.com>
2023-11-16 20:11:20 +0100

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,9 @@
+#ifndef __SECRET_H__
+#define __SECRET_H__
+
+#define SSID_NAME "My SSID"
+#define SSID_PSW "My SSID password"
+#define DEV_ID "Device ID from weathercloud"
+#define DEV_KEY "Key of device"
+
+#endif /* __SECRET_H__ */
